600a4236c8afba2ff422ad79aaf99179890ae9f1
Roel Dingle <rdingle@SAC-LPT-0129.straightarrow.com>
2019-09-19 22:13:27 +0800

    create discussion api file

---
+++ create_discussion.php
@@ -0,0 +1,27 @@
+<?php
+require_once('main.php');
+
+return $_POST;
+
+
+
+// $account_id = 176953;
+// $project_id = 1878;
+
+// $data = array(
+// 	'company' => 'Straightarrow Corporation',
+// 	'app_name' => 'SA-RNL ActiveCollab automation',
+// 	'email' => 'rmdingle@straightarrow.com.ph',
+// 	'password' => 'rinoayuna12',
+// );
+
+// $oMain = new Main();
+
+// $authenticator = $oMain->intializeAuth($data);
+// $token = $oMain->issueToken($authenticator, $account_id);
+// $client = $oMain->createClientInstance($token);
+
+
+// if(isset($_POST)){
+// 	$oMain->createProjectData($client, $project_id, 'discussions', $_POST);
+// }
