b827b13c25bce10cbb78ed0dbd5a3185c31bee59
Roel Dingle <rdingle@SAC-LPT-0129.straightarrow.com>
2019-09-19 23:59:19 +0800

    test

--- create_discussion.php
+++ create_discussion.php
@@ -1,15 +1,14 @@
 <?php
 // required headers
-// header("Access-Control-Allow-Origin: *");
-// header("Content-Type: application/json; charset=UTF-8");
-// header("Access-Control-Allow-Methods: POST");
-// header("Access-Control-Max-Age: 3600");
-// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $data = array(
 	'test1' => 'aaaa',
 	'test2' => 'bbbb'
-
 );
 
 echo json_encode($data);
