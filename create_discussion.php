e410648b6e0e324e965771b263b139dac350c00c
Roel Dingle <rdingle@SAC-LPT-0129.straightarrow.com>
2019-09-20 02:11:38 +0800

    beta test setup

--- create_discussion.php
+++ create_discussion.php
@@ -6,51 +6,47 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-
+require_once('main.php');
 
 $_POST = file_get_contents('php://input');
 $postData = json_decode($_POST);
 
-echo json_encode($postData);
+//echo json_encode($postData);
 
 
 
+$account_id = 176953;
+$project_id = 1878;
 
-// require_once('main.php');
+$data = array(
+	'company' => 'Straightarrow Corporation',
+	'app_name' => 'SA-RNL ActiveCollab automation',
+	'email' => 'rmdingle@straightarrow.com.ph',
+	'password' => 'rinoayuna12',
+);
 
+$oMain = new Main();
 
-// $account_id = 176953;
-// $project_id = 1878;
-
-// $data = array(
-// 	'company' => 'Straightarrow Corporation',
-// 	'app_name' => 'SA-RNL ActiveCollab automation',
-// 	'email' => 'rmdingle@straightarrow.com.ph',
-// 	'password' => 'rinoayuna12',
-// );
-
-// $oMain = new Main();
-
-// $authenticator = $oMain->intializeAuth($data);
-// $token = $oMain->issueToken($authenticator, $account_id);
-// $client = $oMain->createClientInstance($token);
+$authenticator = $oMain->intializeAuth($data);
+$token = $oMain->issueToken($authenticator, $account_id);
+$client = $oMain->createClientInstance($token);
 
 
-// if(isset($postData)){
+if(isset($postData)){
 
-// 	$postData = array(
-// 		'name' => $postData['client_name'],
-// 		'body' => $postData['versions'],
-// 	);
+	$postData = array(
+		'name' => $postData['client_name'],
+		'body' => $postData['versions'],
+	);
 
-// 	$bResponse = $oMain->createProjectData($client, $project_id, 'discussions', $postData);
+	$bResponse = $oMain->createProjectData($client, $project_id, 'discussions', $postData);
 
-// 	if($bResponse){
-// 		$response = 'all good';
-// 	}else{
-// 		$response = 'all failed';
-// 	}
-// }
+	if($bResponse){
+		$response = 'all good';
+	}else{
+		$response = 'all failed';
+	}
+}
 
 
-// echo json_encode($response);
+echo json_encode($response);
