0c37fb69cff6c4afac249b85ccb019bee1fd642d
Roel Dingle <rmdingle@straightarrow.com>
2020-01-14 21:20:19 +0800

    start testing smtp

--- index.php
+++ index.php
@@ -5,3 +5,15 @@ straightarrow-tech.com:2082	straigy4	P@ssw0rd12345
 git pull https://github.com/roeldingle/rnl-api.git master
 */
 
+$f = fsockopen('smtp host', 25) ;
+if ($f !== false) {
+    $res = fread($f, 1024) ;
+    if (strlen($res) > 0 && strpos($res, '220') === 0) {
+        echo "Success!" ;
+    }
+    else {
+        echo "Error: " . $res ;
+    }
+}
+fclose($f) ;
+
