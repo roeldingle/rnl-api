8c6f8a8d0b81063c7c4af9b8259ed6cc22d473e2
Roel Dingle <rmdingle@straightarrow.com>
2020-01-14 21:36:17 +0800

    setup a test gmail email sender

--- index.php
+++ index.php
@@ -1,53 +1,43 @@
 <?php
 
-/*
-straightarrow-tech.com:2082	straigy4	P@ssw0rd12345
-git pull https://github.com/roeldingle/rnl-api.git master
-*/
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
 
-$ports[] = array('host'=>'interspire.smtp.com','number'=>25);
-$ports[] = array('host'=>'interspire.smtp.com','number'=>2525);
-$ports[] = array('host'=>'interspire.smtp.com','number'=>25025);
-$ports[] = array('host'=>'helpme.interspire.smtp.com','number'=>80);
 
-$ports[] = array('host'=>'google.com','number'=>80);
-$ports[] = array('host'=>'smtp.gmail.com','number'=>587);
-$ports[] = array('host'=>'smtp.gmail.com','number'=>465);
-$ports[] = array('host'=>'pop.gmail.com','number'=>995);
-$ports[] = array('host'=>'imap.gmail.com','number'=>993);
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Mailer = "smtp";
 
-$ports[] = array('host'=>'ftp.mozilla.org','number'=>21);
-$ports[] = array('host'=>'smtp2go.com','number'=>8025);
+$mail->SMTPDebug  = 1;  
+$mail->SMTPAuth   = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port       = 587;
+$mail->Host       = "smtp.gmail.com";
+$mail->Username   = "roeldingle@gmail.com";
+$mail->Password   = "gm@il@lb@rnkidd05";
 
-$ports[] = array('host'=>'relay.dnsexit.com','number'=>25);
-$ports[] = array('host'=>'relay.dnsexit.com','number'=>26);
-$ports[] = array('host'=>'relay.dnsexit.com','number'=>940);
-$ports[] = array('host'=>'relay.dnsexit.com','number'=>8001);
-$ports[] = array('host'=>'relay.dnsexit.com','number'=>2525);
-$ports[] = array('host'=>'relay.dnsexit.com','number'=>80);
 
-$ports[] = array('host'=>'mail.authsmtp.com','number'=>23);
-$ports[] = array('host'=>'mail.authsmtp.com','number'=>25);
-$ports[] = array('host'=>'mail.authsmtp.com','number'=>26);
-$ports[] = array('host'=>'mail.authsmtp.com','number'=>2525);
+$mail->IsHTML(true);
+$mail->AddAddress("rmdingle@straightarrow.com.ph", "Roel Dingle");
+$mail->SetFrom("roeldingle@gmail.com", "Roel Test");
+$mail->AddReplyTo("roeldingle@gmail.com", "Roel Test");
+$mail->AddCC("roeldingle@gmail.com", "Roel Test");
+$mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+$content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
 
-foreach ($ports as $port)
-{
-    //$connection = @fsockopen($port['host'], $port['number']);
-    $connection = @fsockopen($port['host'], $port['number'], $errno, $errstr, 5); // 5 second timeout for each port.
 
-    if (is_resource($connection))
-    {
-        echo '<h2>' . $port['host'] . ':' . $port['number'] . ' ' . '(' . getservbyport($port, 'tcp') . ') is open.</h2>' . "\n";
-
-        fclose($connection);
-    }
-
-    else
-    {
-        echo '<h2>' . $port['host'] . ':' . $port['number'] . ' is not responding.</h2>' . "\n";
-    }
+$mail->MsgHTML($content); 
+if(!$mail->Send()) {
+  echo "Error while sending Email.";
+  var_dump($mail);
+} else {
+  echo "Email sent successfully";
 }
+
+
 
 
 echo "tester check smtp 3";
